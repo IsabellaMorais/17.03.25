eb1a8b4f2efda6d18e4ffbf3ef6b390cbf782bc4
IsabellaMorais <isabellamoraismamedi@gmail.com>
2025-03-17 14:40:51 -0300

    Create cliente.cs

---
+++ cliente.cs
@@ -0,0 +1,27 @@
+namespace LHPet.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Key]
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Ib {get; set}
+
+        [Column("Nome")]
+        [Display(Name = "Nome")] 
+        public string Nome{get; set;}
+
+        [Column {"Cpf"}]
+        [Display (Name = "Cpf")]
+        public string Cpf {get; set;}
+
+        [Column {"Email"}]
+        [Display (Name = "Email")]
+        public string Email{get; set;}
+
+        [Column {"Paciente"}]
+        [Display (Name = "Paciente")]
+        public string Paciente{get; set;}
+    }
+}
